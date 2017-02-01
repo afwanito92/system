111df8b767f7e008bcf8ddf543be9464997c2d4e
user <briand78410@gmail.com>
2017-02-01 12:54:55 +0100

    fin 1ere seance

--- TD1/ex3/ex3.c
+++ TD1/ex3/ex3.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #define BUFFER 200
 void create(){
-	FILE *fp;
-	fp =fopen("toto", "w+");
-	fputs("Ceci est un test \n C'est marrant", fp);	
-	fclose(fp);
+	int fp;
+	int c = 65;
+	fp = open("toto", O_RDONLY | O_CREAT, S_IRWXU);
+	write(fp, &c, sizeof(char));	
+	close(fp);
 }
 //on veut copier le texte de toto et l'envoyer dans titi
 int main(int argc, char **argv)
 {
 	//declaration des variables
-	FILE *f;
-	FILE *g;
+	int f;
+	int g;
 	int c;
-	char car[BUFFER] = "";
-	//on ouvre les fichiers titi et toto
-	f = fopen("toto", "r");
-	g = fopen("titi", "w");
 	
-	//Si f est null on créé un fichier toto 
-	if (f == NULL){
+	//on ouvre les fichiers titi et toto
+	f = open("toto", O_RDONLY);
+	
+	//on prepare g de sorte a ce qu'on puisse creer un fichier
+	g = open("titi", O_CREAT | O_WRONLY, S_IRWXU);
+	
+	//Tant que f = -1  on créé un fichier toto 
+	while (f == -1){
+		close(f);
 		create();
+		f = open("toto", O_RDONLY);
+	
 	}
+	
 	//ensuite on lit ligne par ligne 
-	while (fgets(car, BUFFER, f) != NULL){
-		fputs(car, g);
+	
+	while (read(f, &c, sizeof(char))){
+		write(g, &c, sizeof(char));
 	}
-	fclose(f);
-	fclose(g);
+	printf("a");
+	close(f);
+	close(g);
 	return 0;
 }
 
