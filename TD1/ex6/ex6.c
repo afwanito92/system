cf01c157cd56ab4b1d9b46c040df33cbe1dff6c2
Corentin <briand78410@gmail.com>
2017-02-08 11:05:23 +0100

    ex6 fin

--- TD1/ex6/ex6.c
+++ TD1/ex6/ex6.c
@@ -25,17 +25,20 @@ int main(int argc, char **argv)
   //la fonction stat a le prototype suivant
   // int stat (const char * path, struct stat * buf)
 
-  // si il y a une erreur on quitte
+  // si il y a une erreur on quittei
   if (stat(argv[1], &fstat) < 0){
     perror("stat");
-    return 1;
+    return 1;i
 
   }
-
+i
   //sinon c'est gagne on affiche
   //regarde si c'est un dossier
   printf( (S_ISDIR(fstat.st_mode) ) ? "d" : "-");
   //on regarde les permissions utilisateur
+  //quand on a xxx & yyy on a la verification du masque yyy sur xxx
+  //ex if (attr.st_mode &(S_IRUSR | S_IWUSR)) --> on verifie deux masque sur st_mode en même temps
+  
   printf( (fstat.st_mode & S_IRUSR) ? "r" : "-");
   printf( (fstat.st_mode & S_IWUSR) ? "w" : "-");
   printf( (fstat.st_mode & S_IXUSR) ? "x" : "-");
